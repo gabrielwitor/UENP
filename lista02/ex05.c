8e46f5da955bda3ca155747e75d50b69cfce89ef
Gabriel Witor <witor.alm@gmail.com>
2023-05-04 10:20:45 -0300

    ex04

---
+++ lista02/ex05.c
@@ -0,0 +1,11 @@
+/*
+Faça um programa que receba como entrada 3 números reais maiores que zero correspondendo
+ao comprimento dos lados do triângulo, em seguida, classifique-o em:
+1. equilátero (os trêes lados de comprimentos iguais)
+2. isósceles (dois lados de comprimentos iguais) ou
+3. escaleno (os três lados de comprimentos diferentes)
+*/
+
+#include <stdio.h>
+
+int main()
