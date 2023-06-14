dbd78ec584702382dbdd9987b0610f082b33e02f
Gabriel Witor <gabriel_witorz@outlook.com>
2023-06-14 09:01:03 -0300

    criacaopastas

---
+++ cursos/udemyC/aulas/programa1.c
@@ -0,0 +1,8 @@
+// aula sobre variáveis
+
+#include <stdio.h> // biblioteca standart in out, ou seja, necessária para entrada e saída de dados.
+
+int main(){
+    printf("Geek University");
+    return 0;
+}
