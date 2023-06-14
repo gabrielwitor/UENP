273b83d66b8d671048071ee331c6a19024a4f4ae
Gabriel Witor <gabriel_witorz@outlook.com>
2023-06-14 09:54:49 -0300

    program1

--- cursos/udemyC/aulas/programa1.c
+++ cursos/udemyC/aulas/programa1.c
@@ -2,7 +2,14 @@
 
 #include <stdio.h> // biblioteca standart in out, ou seja, necessária para entrada e saída de dados.
 
-int main(){
-    printf("Geek University");
+int main(){ // a chave delimita um bloco de código (inicio do bloco)
+    int idade; //declarando variável 
+
+    printf("Qual e a sua idade? "); //função para escrever algo na saída padrão (console)
+
+    scanf("%d",&idade); //recebendo dados
+
+    printf("Voce tem %i anos.",idade); // o ponto e vírgula finaliza um comando
     return 0;
-}
+    
+} // fim do bloco
