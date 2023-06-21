3d5b4833cf1ce6c1c108013c787c60838e12b50d
Gabriel Witor <gabriel_witorz@outlook.com>
2023-06-21 10:17:26 -0300

    commit

---
+++ aulas/aula03.c
@@ -0,0 +1,12 @@
+/*
+Aula do dia 21/06/2023
+*/
+
+#include <stdio.h>
+
+int main(){
+
+
+
+    return 0;
+}
---
+++ udemyC/EXS03/ex19.c
@@ -0,0 +1,17 @@
+/*
+Leia um valor de volume em litros e apresente-o convertido em metros cúbicos m³.
+A fórmula de conversão é: M = L / 1000, sendo L o volume em litros e M o volume
+em metros cúbicos.
+*/
+
+#include <stdio.h>
+
+int main(){
+
+    float m, l;
+
+    printf("Digite o volume em litros: ");
+    scanf("%f",&l);
+
+    return 0;
+}
