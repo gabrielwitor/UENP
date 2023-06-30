b0cbf1d1d40b2e984226856e1984f10420f6c19b
Gabriel Witor <gabriel_witorz@outlook.com>
2023-06-30 18:35:02 -0300

    Commit

---
+++ extra-curricular/udemyC/EXS03/ex21.c
@@ -0,0 +1,14 @@
+/*
+Leia um valor de massa em libras e apresente-o convertido em quilogramas. A fórmula de conversão é:
+K = L * 0.45, sendo K a massa em quilogramas e L a massa em libras.
+*/
+
+#include <stdio.h>
+
+int main(){
+
+    
+
+
+    return 0;
+}
