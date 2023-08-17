e4c68055a5da1b85ba97dd8adbacca33ebc0d286
Gabriel Witor <witor.alm@gmail.com>
2023-08-17 12:08:05 -0300

    laobratório

---
+++ laboratorios/lab7/ex05.py
@@ -0,0 +1,10 @@
+"""
+Uma outra tarefa que já podemos fazer é imprimir os números em ordem decrescente. 
+Faça um código na célula abaixo que imprima todos os
+números inteiros de 25 até -10(intervalo fechado!):
+"""
+
+lista = list(range(25,-11,-1))
+
+for i in lista:
+    print(i)
